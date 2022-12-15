26ee7bde6377e7b1c2df0f76fb253a5984c9629a
reneenh <reneehamlinteaches@gmail.com>
2022-12-14 21:58:10 -0800

    worked on user routes. needs work.

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,1 +1,17 @@
 // call on functions created in the Controllers; those are the "actual" routes.
+
+function getUser() {
+    // get user id.
+    const searchParams = new URLSearchParams(document.location.search.substring(1));
+    const userId = searchParams.get('id');
+  
+    // get pizzaInfo
+    fetch(`/api/users/${UserId}`)
+      .then(response => {
+        console.log(response);
+        return response.json();
+      })
+      .then(printUser);
+  };
+
+  
