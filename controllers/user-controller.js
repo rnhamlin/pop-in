bea3aa335620fc83ae48b3a5a055b09096767986
reneenh <reneehamlinteaches@gmail.com>
2022-12-14 21:42:09 -0800

    filled out more of the user-controller file

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,56 +1,78 @@
 // user controller
 
 //import model
-const {Thought, User} = require("../models");
+const { Thought, User } = require("../models");
 
 // get all users
-getAllUsers(req, res) {
-    User.find({}).then(dbUserData => res.json(dbUserData)).catch(err => {
-        console.log(err);
-        res.status(400).json(err);
+getAllUsers(req, res);
+{
+  User.find({})
+    .then((dbUserData) => res.json(dbUserData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
     });
-};
+}
 
 // get one user by id
-getUserById({ params }, res) {
-    User.findOne({ _id: params.id}).then(dbUserData => {
-        // send error message if no user is found
-        if (!dbUserData) {
-            res.json(404).json({message: "No username found with this id!"});
-            return;
-        }
-        res.json(dbUserData);
-    }).catch(err => {
-        console.log(err);
-        res.status(400).json(err);
+getUserById({ params }, res);
+{
+  User.findOne({ _id: params.id })
+    .then((dbUserData) => {
+      // send error message if no user is found
+      if (!dbUserData) {
+        res.json(404).json({ message: "No username found with this id!" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
     });
 }
 
 // create a user
-createUser({body}, res) {
-    User.createUser(body).then((user) => res.json(user));
-};
-
-
+createUser({ body }, res);
+{
+  User.createUser(body)
+    .then((user) => res.json(user))
+    .catch((err) => res.status(400).json(err));
+}
 
 // update user by id
-
-
+updateUser({ params, body }, res);
+{
+  User.findOneandUpdate({ _id: params.id }, body, { new: true })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id!" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => res.status(400).json(err));
+}
 
 // delete user by id
-
-
+deleteUser({ params }, res);
+{
+  User.findOneAndDelete({ _id: params.id })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id!" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => res.status(400).json(err));
+}
 
 // BONUS: remove a user's associated thoughts when user is deleted
 
-
-
 // create friend
 
-
-
 // delete friend
-
 
 // export code
 
