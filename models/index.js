8f83b43f4e5d97d1b77ee26d4d0c256ec664215a
reneenh <reneehamlinteaches@gmail.com>
2022-12-17 14:40:01 -0800

    edited models/index.js per AskBCS recommendation.

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 const User = require("./User.js");
 const Thought = require("./Thought.js");
-const Reaction = require("./Reaction.js");
 //I don't need one for Reaction because it will only be a schema, not a model.
 
 User.index({ name: 1, type: -1 }).get(function () {
@@ -10,4 +9,4 @@ User.index({ email: 1, type: -1 }).get(function () {
   return this.User;
 });
 
-module.exports = { User, Thought, Reaction };
+module.exports = { User, Thought };
